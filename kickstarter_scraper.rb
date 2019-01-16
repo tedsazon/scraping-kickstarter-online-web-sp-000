076fbcc0eb29fc8dec8e455ba509493662507141
Ted Sazon <tedsazon@gmail.com>
2019-01-16 20:33:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,7 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value
+      :description => project.css("p.bbcard_blurb").text
     }
   end
   projects
