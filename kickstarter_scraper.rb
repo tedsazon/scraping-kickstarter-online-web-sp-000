2f4772b0905ec3eb149c602950ec67e6bd26f038
Ted Sazon <tedsazon@gmail.com>
2019-01-16 20:25:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,7 @@ binding.pry
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
 
 
 create_project_hash
