b5717181370f851e141e9cf8008a36e54c4f6b6e
Ted Sazon <tedsazon@gmail.com>
2019-01-16 20:33:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,6 +12,8 @@ def create_project_hash
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value
       :description => project.css("p.bbcard_blurb").text
+      :location => project.css("ul.project-meta").text
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
   projects
