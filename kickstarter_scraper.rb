75833315f982e9a55eadf8139c7644a26ba160ec
Ted Sazon <tedsazon@gmail.com>
2019-01-16 20:30:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,9 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
 end
 binding.pry
 
