be4cfc123a3d591da20ea0c290800b05987ad8e1
Ted Sazon <tedsazon@gmail.com>
2019-01-16 20:20:36 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,11 +1,2 @@
 require 'nokogiri'
 require 'pry'
-
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-
-  kickstarter = Nokogiri::HTML(html)
-end
-
-create_project_hash
-binding.pry
